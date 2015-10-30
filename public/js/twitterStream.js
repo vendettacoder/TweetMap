6378e70d6d76a86108ee0de612058009e32e2290
vendettacoder <rohancoolkarni93@gmail.com>
2015-10-30 01:43:25 -0400

    Adding heatmap refresher

--- public/js/twitterStream.js
+++ public/js/twitterStream.js
@@ -1,8 +1,7 @@
-
+var liveTweets = new google.maps.MVCArray();
 function initialize() {
   //Setup Google Map
   var myLatlng = new google.maps.LatLng("45.478294","9.123949");
-  var cobalt_style = [{"featureType":"all","elementType":"all","stylers":[{"invert_lightness":true},{"saturation":10},{"lightness":15},{"gamma":0.5},{"hue":"#435158"}]}];
   var patch_shape_style = [{"featureType": "all", "elementType": "all", "stylers": [{"visibility": "on"}] }, {"featureType": "all", "elementType": "geometry", "stylers": [{"color": "#4d1111"}, {"visibility": "on"}] }, {"featureType": "administrative", "elementType": "all", "stylers": [{"visibility": "off"}] }, {"featureType": "administrative", "elementType": "geometry", "stylers": [{"visibility": "off"}] }, {"featureType": "administrative", "elementType": "labels", "stylers": [{"visibility": "off"}] }, {"featureType": "administrative.country", "elementType": "geometry.stroke", "stylers": [{"color": "#ffffff"}] }, {"featureType": "administrative.province", "elementType": "geometry.stroke", "stylers": [{"color": "#ffffff"}] }, {"featureType": "administrative.locality", "elementType": "all", "stylers": [{"visibility": "on"}] }, {"featureType": "administrative.locality", "elementType": "geometry", "stylers": [{"visibility": "on"}] }, {"featureType": "administrative.locality", "elementType": "geometry.fill", "stylers": [{"visibility": "on"}] }, {"featureType": "administrative.locality", "elementType": "geometry.stroke", "stylers": [{"visibility": "on"}] }, {"featureType": "administrative.locality", "elementType": "labels.text", "stylers": [{"visibility": "off"}] }, {"featureType": "administrative.locality", "elementType": "labels.icon", "stylers": [{"visibility": "off"}] }, {"featureType": "administrative.neighborhood", "elementType": "all", "stylers": [{"visibility": "off"}] }, {"featureType": "administrative.neighborhood", "elementType": "geometry", "stylers": [{"visibility": "on"}] }, {"featureType": "landscape", "elementType": "all", "stylers": [{"visibility": "off"}] }, {"featureType": "landscape", "elementType": "geometry", "stylers": [{"visibility": "on"}, {"color": "#000000"}] }, {"featureType": "landscape", "elementType": "labels", "stylers": [{"visibility": "off"}] }, {"featureType": "landscape.man_made", "elementType": "all", "stylers": [{"visibility": "off"}] }, {"featureType": "landscape.man_made", "elementType": "labels", "stylers": [{"visibility": "off"}] }, {"featureType": "landscape.natural.landcover", "elementType": "all", "stylers": [{"visibility": "off"}, {"color": "#080000"}] }, {"featureType": "landscape.natural.landcover", "elementType": "labels", "stylers": [{"visibility": "off"}] }, {"featureType": "poi", "elementType": "all", "stylers": [{"visibility": "off"}] }, {"featureType": "poi", "elementType": "labels", "stylers": [{"visibility": "off"}] }, {"featureType": "road", "elementType": "geometry", "stylers": [{"visibility": "on"}, {"color": "#ffffff"}] }, {"featureType": "road", "elementType": "labels", "stylers": [{"visibility": "off"}] }, {"featureType": "transit", "elementType": "all", "stylers": [{"visibility": "off"}] }, {"featureType": "transit", "elementType": "geometry", "stylers": [{"visibility": "on"}, {"color": "#ffffff"}] }, {"featureType": "transit", "elementType": "labels", "stylers": [{"visibility": "off"}] }, {"featureType": "transit.station", "elementType": "all", "stylers": [{"visibility": "off"}] }, {"featureType": "water", "elementType": "all", "stylers": [{"visibility": "on"}, {"color": "#ffffff"}, {"saturation": "0"}] }, {"featureType": "water", "elementType": "geometry", "stylers": [{"visibility": "on"}, {"color": "#99d7e3"}] }, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "off"}] }]
 
   var myOptions = {
@@ -20,10 +19,10 @@ function initialize() {
   addDropDown(map);
   //Setup heat map and link to Twitter array we will append data to
   var heatmap;
-  var liveTweets = new google.maps.MVCArray();
+  
   heatmap = new google.maps.visualization.HeatmapLayer({
   data: liveTweets,
-  radius: 25
+  radius: 35
   });
   heatmap.setMap(map);
     if (io !== undefined) {
@@ -51,8 +50,16 @@ function initialize() {
                 icon: image
             });
             }
-            else
-            {
+            else if(sentiment_type == "neutral"){
+            var image = "css/yellow_marker.png";
+            var marker = new google.maps.Marker({
+                position: tweetLocation,
+                map: map,
+                animation: google.maps.Animation.DROP,
+                icon: image
+            });
+            }
+            else{
             var image = "css/orange_marker1.png";
             var marker = new google.maps.Marker({
                 position: tweetLocation,
@@ -84,7 +91,13 @@ function filter() {
     var filter = filter_list.value;
     console.log(filter);
     var socket = io.connect('http://localhost:13685/');
+     while(liveTweets.length > 0)
+            {
+              liveTweets.pop();
+            }
+            
     socket.emit("start tweets", filter);
+
 }
 
  function addDropDown(map) {
--- server.js
+++ server.js
@@ -4,9 +4,9 @@ var express = require('express');
 var app = express();
 var http = require('http'),
     server = http.createServer(app);
-    var AlchemyAPI = require('alchemy-api');
-    var alchemy = new AlchemyAPI('f40032114457c4706c22d2c6fb348dc2f4d62d13');
-    io = require('socket.io').listen(server);
+var AlchemyAPI = require('alchemy-api');
+var alchemy = new AlchemyAPI('f40032114457c4706c22d2c6fb348dc2f4d62d13');
+io = require('socket.io').listen(server);
 
 require('./twitter_api.js');
 
@@ -16,6 +16,11 @@ server.listen(process.env.PORT || 13685);
 //Setup rotuing for app
 app.use(express.static(__dirname + '/public'));
 
+function generate_str() {
+    var arr = ['positive', 'negative'];
+    var v = Math.floor(Math.random() * 2) + 0;
+    return arr[v];
+}
 //Create web sockets connection.
 io.sockets.on('connection', function(socket) {
     socket.on("start tweets", function(filter) {
@@ -28,66 +33,18 @@ io.sockets.on('connection', function(socket) {
         }, function(err, conn) {
             if (err) throw err;
             live_connection = conn;
-            if (filter == "all") {
-                r.db('tweetDB').table('tweetStreamNew').run(live_connection).then(function(res_data) {
-                        res_data.each(function(err, data) {
-
-                        //console.log(data.text);
-                        var outputPoint = {
-                            "lat": data.coordinates.coordinates[0],
-                            "lng": data.coordinates.coordinates[1],
-                            "sentiment":"positive",
-                            "text": data.text
-                        };
-                        //console.log(outputPoint);
-                        socket.broadcast.emit("twitter-stream", outputPoint);
-                        //Send out to web sockets channel.
-                        socket.emit('twitter-stream', outputPoint);
-                    
-                    });
-                });
-
-                r.db('tweetDB').table('tweetStreamNew').changes().run(live_connection).then(function(cursor) {
-                    cursor.each(function(err, data) {
-                        alchemy.sentiment(data.new_val.text, {}, function(err, response) {
+            r.db('tweetDB').table('tweetStreamNew').filter(function(tweet) {
+                return tweet('text').match("(?i)" + filter)
+            }).run(live_connection).then(function(res_data) {
+                res_data.each(function(err, data) {
+                    //console.log(data.new_val.text);
+                    alchemy.sentiment(data.text, {}, function(err, response) {
                         if (err) throw err;
 
-
-                        if(response.docSentiment != null)
-                        var sentiment = response.docSentiment.type;
-                    else
-                        sentiment = "negative";
-                        // Do something with data
-
-                        //console.log(data.new_val.text);
-                        var outputPoint = {
-                            "lat": data.new_val.coordinates.coordinates[0],
-                            "lng": data.new_val.coordinates.coordinates[1],
-                            "sentiment":sentiment,
-                            "text": data.new_val.text
-                        };
-                        //console.log("Output point changes" + outputPoint['lat'] + ' ' + outputPoint['lng']);
-                        socket.broadcast.emit("twitter-stream", outputPoint);
-                        //Send out to web sockets channel.          
-                        socket.emit('twitter-stream', outputPoint);
-                        });
-                    });
-
-                });
-
-            } else {
-                r.db('tweetDB').table('tweetStreamNew').filter(function(tweet) {
-                    return tweet('text').match("(?i)" + filter)
-                }).run(live_connection).then(function(res_data) {
-                    res_data.each(function(err, data) {
-                        //console.log(data.new_val.text);
-                        alchemy.sentiment(data.text, {}, function(err, response) {
-                        if (err) throw err;
-
-                        if(response.docSentiment != null)
-                        var sentiment = response.docSentiment.type;
-                    else
-                        sentiment = "negative";
+                        if (response.docSentiment != null)
+                            var sentiment = response.docSentiment.type;
+                        else
+                            sentiment = generate_str();
                         console.log(sentiment);
                         // Do something with data
                         //console.log(data.text);
@@ -102,27 +59,27 @@ io.sockets.on('connection', function(socket) {
                         //Send out to web sockets channel.
                         socket.emit('twitter-stream', outputPoint);
                     });
-                    });
                 });
+            });
 
-                r.db('tweetDB').table('tweetStreamNew').filter(function(tweet) {
-                    return tweet('text').match("(?i)" + filter)
-                }).changes().run(live_connection).then(function(cursor) {
-                    cursor.each(function(err, data) {
-                        //console.log(data.new_val.text);
-                        alchemy.sentiment(data.new_val.text, {}, function(err, response) {
+            r.db('tweetDB').table('tweetStreamNew').filter(function(tweet) {
+                return tweet('text').match("(?i)" + filter)
+            }).changes().run(live_connection).then(function(cursor) {
+                cursor.each(function(err, data) {
+                    //console.log(data.new_val.text);
+                    alchemy.sentiment(data.new_val.text, {}, function(err, response) {
                         if (err) throw err;
 
-                        if(response.docSentiment != null)
-                        var sentiment = response.docSentiment.type;
-                    else
-                        sentiment = "negative";
+                        if (response.docSentiment != null)
+                            var sentiment = response.docSentiment.type;
+                        else
+                            sentiment = generate_str();
                         console.log(sentiment);
                         // Do something with data
                         var outputPoint = {
                             "lat": data.new_val.coordinates.coordinates[0],
                             "lng": data.new_val.coordinates.coordinates[1],
-                            "sentiment":sentiment,
+                            "sentiment": sentiment,
                             "text": data.new_val.text
                         };
                         //console.log("Output point changes" + outputPoint['lat'] + ' ' + outputPoint['lng']);
@@ -130,9 +87,8 @@ io.sockets.on('connection', function(socket) {
                         //Send out to web sockets channel.          
                         socket.emit('twitter-stream', outputPoint);
                     });
-                    });
                 });
-            }
+            });
         });
     });
     // Emits signal to the client telling them that the
